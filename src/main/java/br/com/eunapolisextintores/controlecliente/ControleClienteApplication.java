0972ed14b284cf9b7c0b5acb91fd5b31c1d3daac
Guilherme Santos <guilhermesj.dev@gmail.com>
2023-05-23 11:42:50 -0300

    implementa API de test

--- src/main/java/br/com/eunapolisextintores/controlecliente/ControleClienteApplication.java
+++ src/main/java/br/com/eunapolisextintores/controlecliente/ControleClienteApplication.java
@@ -2,9 +2,19 @@ package br.com.eunapolisextintores.controlecliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class ControleClienteApplication {
+
+	@GetMapping
+	public String getHomeTest() {
+		return "API CONTROLE CLIENTE TEST";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleClienteApplication.class, args);
