dd49dd9a1a7d6b0400cce1ae61aa1c6a77097f56
Guilherme Santos <guilhermesj.dev@gmail.com>
2023-05-23 10:49:53 -0300

    inicia projeto

---
+++ src/main/java/br/com/eunapolisextintores/controlecliente/ControleClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.eunapolisextintores.controlecliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleClienteApplication.class, args);
+	}
+
+}
